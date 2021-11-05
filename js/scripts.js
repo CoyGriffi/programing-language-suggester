392c54624bfeb6450745aff9ceed4feb04cabb6d
coygriffi <coygriffi@gmail.com>
2021-11-05 16:40:22 -0700

    add function to determin language depending on total value. not working, debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
+function questionTotal (number1, number2, number3, number4, number5) {
+  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4) + parseInt(number5)
+}
+
+
 $(document).ready(function() {
-  $("#question1").change(function(event) {
+  $("#radioQuestions").submit(function(event) {
     event.preventDefault();
+  let numberTotal = questionTotal (platform, game, color, city, computer);
   const platform = $("input:radio[name=platform]:checked").val();
   const game = $("input:radio[name=game]:checked").val();
   const color = $("input:radio[name=color]:checked").val();
   const city = $("input:radio[name=city]:checked").val();
   const computer = $("input:radio[name=computer]:checked").val();
-
   
+  if (numberTotal > 1) {
+    $("#q1prompt").show();
+  }
   });
 });
