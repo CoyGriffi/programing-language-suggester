446d0ffc53cda20b3fe6829adcb25a8ae968c6e1
coygriffi <coygriffi@gmail.com>
2021-11-04 21:04:40 -0700

    add js to declare radio variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
+  const platform = $("input:radio[name=platform]:checked").val();
+  const game = $("input:radio[name=game]:checked").val();
+  const color = $("input:radio[name=color]:checked").val();
+  const city = $("input:radio[name=city]:checked").val();
+  const computer = $("input:radio[name=computer]:checked").val();
   
-}
+});
