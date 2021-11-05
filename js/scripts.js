584a63a3e6a950a97b703e7c41f337d4a9ca8bf6
coygriffi <coygriffi@gmail.com>
2021-11-05 14:23:24 -0700

    add js for q1 logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+  $("#question1").change(function(event) {
+    event.preventDefault();
   const platform = $("input:radio[name=platform]:checked").val();
   const game = $("input:radio[name=game]:checked").val();
   const color = $("input:radio[name=color]:checked").val();
   const city = $("input:radio[name=city]:checked").val();
   const computer = $("input:radio[name=computer]:checked").val();
 
-  if (platform === "windows" ) {
-    $("").show();
-  }
+  if (platform === "mac" ) {
+    $("#q1prompt").show();
+    };
+  });
 });
