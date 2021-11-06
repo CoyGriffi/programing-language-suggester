6fd15996034e7a5a7fba4228da1b8e39e2ddc690
coygriffi <coygriffi@gmail.com>
2021-11-05 19:10:51 -0700

    fix js bugs and test program for additional bugs

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function questionTotal (number1, number2, number3, number4, number5) {
 
 $(document).ready(function() {
   $("#radioQuestions").submit(function(event) {
-
+    event.preventDefault();
   let platform = $("input:radio[name=platform]:checked").val();
   let game = $("input:radio[name=game]:checked").val();
   let color = $("input:radio[name=color]:checked").val();
@@ -14,18 +14,17 @@ $(document).ready(function() {
   let numberTotal = questionTotal (platform, game, color, city, computer);
 
   if (numberTotal >= 15) {
-    $("#q2prompt, q3prompt, q5prompt").hide();
+    $("#q2prompt, #q3prompt, #q5prompt").hide();
     $("#q1prompt").show();
     } else if (numberTotal <= 14 && numberTotal >= 7) {
-      $("#q1prompt, q3prompt, q5prompt").hide();
+      $("#q1prompt, #q3prompt, #q5prompt").hide();
       $("#q2prompt").show();
-    } else if (numberTotal <= 6 && numberTotal >= 1) {
-      $("#q2prompt, q1prompt, q5prompt").hide();
+    } else if (numberTotal <= 6 && numberTotal >= 2) {
+      $("#q2prompt, #q1prompt, #q5prompt").hide();
       $("#q3prompt").show();
-    } else if (numberTotal < 0) {
-      $("#q2prompt, q3prompt, q1prompt").hide();
+    } else if (numberTotal < 1) {
+      $("#q2prompt, #q3prompt, #q1prompt").hide();
       $("#q5prompt").show();
     }
-    event.preventDefault();
   });
 });
