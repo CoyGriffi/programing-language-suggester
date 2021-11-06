d58f8f6d76d1be1560630c1ad906eecda5fad0d1
coygriffi <coygriffi@gmail.com>
2021-11-05 19:00:32 -0700

    add js code to determin user output on submit. bug in code relating to value less than 0

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,27 @@ function questionTotal (number1, number2, number3, number4, number5) {
 
 $(document).ready(function() {
   $("#radioQuestions").submit(function(event) {
-    event.preventDefault();
+
+  let platform = $("input:radio[name=platform]:checked").val();
+  let game = $("input:radio[name=game]:checked").val();
+  let color = $("input:radio[name=color]:checked").val();
+  let city = $("input:radio[name=city]:checked").val();
+  let computer = $("input:radio[name=computer]:checked").val();
   let numberTotal = questionTotal (platform, game, color, city, computer);
-  const platform = $("input:radio[name=platform]:checked").val();
-  const game = $("input:radio[name=game]:checked").val();
-  const color = $("input:radio[name=color]:checked").val();
-  const city = $("input:radio[name=city]:checked").val();
-  const computer = $("input:radio[name=computer]:checked").val();
-  
-  if (numberTotal > 1) {
+
+  if (numberTotal >= 15) {
+    $("#q2prompt, q3prompt, q5prompt").hide();
     $("#q1prompt").show();
-  }
+    } else if (numberTotal <= 14 && numberTotal >= 7) {
+      $("#q1prompt, q3prompt, q5prompt").hide();
+      $("#q2prompt").show();
+    } else if (numberTotal <= 6 && numberTotal >= 1) {
+      $("#q2prompt, q1prompt, q5prompt").hide();
+      $("#q3prompt").show();
+    } else if (numberTotal < 0) {
+      $("#q2prompt, q3prompt, q1prompt").hide();
+      $("#q5prompt").show();
+    }
+    event.preventDefault();
   });
 });
